cc92ed4fdf75fbeb54b4c3d7fa090f36cb15d77a
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-07-05 18:34:06 -0600

    Add second test

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 import chai from "chai";
 import $ from "jquery";
 import View from '../lib/view';
+import Grid from '../lib/grid';
 
 const expect = chai.expect;
 
@@ -14,5 +15,22 @@ describe('my test suite', function () {
     expect(instance).to.be.true;
   });
 
-  
+  it('divides canvas in even grid', function () {
+    const numberOfElements = 5;
+    const grid = new Grid(numberOfElements);
+
+    expect(grid.boxes.length).to.eql(numberOfElements);
+    // expect(grid.boxes[0]).to.kindOf(Box);
+  });
+
+  // Box WIP test:
+  // it('box', function() {
+  //   const x1 = 4;
+  //   const y1 = 3;
+  //   const x2 = 8;
+  //   const y2 = 7;
+  //   const box = new Box(x1, y1, x2, y2);
+  //
+  //   expect(box).?
+  // });
 });
