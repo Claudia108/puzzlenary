d6848cb858ae5693c831a2bf47e7ab9ac748a48f
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-07-12 17:40:06 -0600

    Add start game test

--- test/game-test.js
+++ test/game-test.js
@@ -4,20 +4,40 @@ import $ from "jquery";
 const expect = chai.expect;
 
 describe('game', function () {
+  after(function() {
+    $('.game-table').remove();
+  });
+
   context('start', function () {
-    xit('starts the game', function () {
-      const game = new Game({});
+    it('starts the game', function () {
+      const game = new Game(2, 2);
+
+      game.start();
+
+      expect($('.game-table').length).to.equal(1);
+      expect(game.clicks).to.equal(0);
+      expect(game.interval > 0).to.equal(true);
+    });
+
+    it('starts click event handlers', function () {
+      const game = new Game(2, 2);
+
+      game.start();
+      $('.game-table td').first().trigger('click');
+
+      expect(game.clicks).to.equal(1);
     });
   });
 
-  context('restart', function () {
-    it("resets the game's state, and restarts it", function () {
+  context('end', function () {
+    it("resets the game's state", function () {
       const game = new Game(2, 2);
       game.start();
       $('.game-table td').first().trigger('click', 'td');
 
-      game.restart();
+      game.end();
 
+      expect($('.game-table').length).to.equal(0);
       expect(game.clicks).to.equal(0);
     });
   });
