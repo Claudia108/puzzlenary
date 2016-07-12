bacb9fd0d71452e761de19c2277d47d88f5447d1
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-07-12 17:12:45 -0600

    First game test passing

--- test/game-test.js
+++ test/game-test.js
@@ -1,11 +1,24 @@
 import chai from 'chai';
 import Game from '../lib/game';
+import $ from "jquery";
 const expect = chai.expect;
 
 describe('game', function () {
   context('start', function () {
     xit('starts the game', function () {
-      let game = new Game({});
+      const game = new Game({});
+    });
+  });
+
+  context('restart', function () {
+    it("resets the game's state, and restarts it", function () {
+      const game = new Game(2, 2);
+      game.start();
+      $('.game-table td').first().trigger('click', 'td');
+
+      game.restart();
+
+      expect(game.clicks).to.equal(0);
     });
   });
 });
