7af62c9f954e6b68da015e768db0cf02ae0316d8
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-07-12 18:08:23 -0600

    Add helpers classToggler test for invalid click

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -5,7 +5,66 @@ import Cell from '../lib/cell';
 const expect = chai.expect;
 
 describe('helpers', function () {
-  context('function endPlayArray', function () {
+  describe('classToggler', function () {
+    context("when it's an invalid click", function () {
+      context("with two lives", function () {
+        it('keeps only one life', function () {
+          const el = {className: undefined};
+          const game = {
+            lives: ["life", "lost_life"],
+            isInvalidClick: function(arg) {
+              return true;
+            }
+          }
+          const gameLifeCycle = undefined;
+
+          classToggler(el, game, gameLifeCycle);
+
+          expect(game.lives).to.eql(["life"]);
+        });
+      });
+
+      context("with one life", function () {
+        it('loses all lives', function () {
+          const el = {className: undefined};
+          const game = {
+            lives: ["lost_life"],
+            isInvalidClick: function(arg) {
+              return true;
+            }
+          }
+          const gameLifeCycle = {
+            emit: function() {}
+          };
+
+          classToggler(el, game, gameLifeCycle);
+
+          expect(game.lives).to.eql([]);
+        });
+      });
+
+      context("with no lives", function () {
+        it('loses all lives', function () {
+          const el = {className: undefined};
+          const game = {
+            lives: [],
+            isInvalidClick: function(arg) {
+              return true;
+            }
+          }
+          const gameLifeCycle = {
+            emit: function() {}
+          };
+
+          classToggler(el, game, gameLifeCycle);
+
+          expect(game.lives).to.eql([]);
+        });
+      });
+    });
+  });
+
+  context('endPlayArray', function () {
     xit('removes elements from the array', function () {
       const cellOne = new Cell(1);
       const cellTwo = new Cell(2);
